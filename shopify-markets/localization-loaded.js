0428ebdf2d5a486e714e2e59ee45d42fdc66edcd
Mailmeisters-Dev <99658952+Mailmeisters-Dev@users.noreply.github.com>
2026-01-07 15:53:59 +0100

    Changed event to new name

--- shopify-markets/localization-loaded.js
+++ shopify-markets/localization-loaded.js
@@ -3,7 +3,7 @@ var localization = {{DLV - Shopify Localization}};
 var url = {{Page Hostname}} + {{Page Path}};
 var fullUrl = {{Page URL}};
 
-klaviyo.track('Viewed Page - MM', {
+klaviyo.track('Loaded Shopify Localization - MM', {
     'URL': url,
     'FullURL': fullUrl,
     'Localization': localization
